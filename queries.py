67977d92bac9de47e73cf45f850e429e125b92c1
Alex Diaz <alexdiaz@Alexs-MacBook-Air.local>
2024-12-01 11:41:03 -0600

    updated queries

--- queries.py
+++ queries.py
@@ -1,11 +1,11 @@
 CreateTable = """DROP TABLE IF EXISTS habits;
                 CREATE TABLE habits (
-                    id INT PRIMARY KEY,
+                    id INT AUTOINCREMENT PRIMARY KEY,
                     Task VARCHAR(255),
                     Desc VARCHAR(255),
                     Freq VARCHAR(255)
                 );
                 """
-AddTask = "INSERT INTO habits VALUES (id, task, desc, freq)"
-DeleteTask = "DELETE FROM habits WHERE id=input"
+AddTask = "INSERT INTO habits (Task, Description, Frequency) VALUES (%s, %s, %s);"
+DeleteTask = "DELETE FROM habits WHERE id=%s"
 ShowTasks = "SELECT * FROM habits"
