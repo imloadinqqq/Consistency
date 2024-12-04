aeef3dff03eae37d0ccc89130b4a4447363ca9e9
Alex Diaz <imloadinq7579@gmail.com>
2024-12-04 15:01:38 -0600

    AUTO_INCREMENT id, fixes

--- queries.py
+++ queries.py
@@ -1,11 +1,12 @@
-CreateTable = """DROP TABLE IF EXISTS habits;
-                CREATE TABLE habits (
-                    id INT AUTOINCREMENT PRIMARY KEY,
+CreateTable = """USE habits;
+                DROP TABLE IF EXISTS tasks;
+                CREATE TABLE tasks (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
                     Task VARCHAR(255),
-                    Desc VARCHAR(255),
+                    Des VARCHAR(255),
                     Freq VARCHAR(255)
                 );
                 """
-AddTask = "INSERT INTO habits (Task, Description, Frequency) VALUES (%s, %s, %s);"
-DeleteTask = "DELETE FROM habits WHERE id=%s"
-ShowTasks = "SELECT * FROM habits"
+AddTask = "INSERT INTO tasks (Task, Des, Freq) VALUES (%s, %s, %s);"
+DeleteTask = "DELETE FROM tasks WHERE id=%s"
+ShowTasks = "SELECT * FROM tasks"
